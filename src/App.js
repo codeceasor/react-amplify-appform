c436bf6917543f8ab71789b51e0e4752b1b77133
codeceasor <simransharma271196@gmail.com>
2021-03-25 12:25:22 +0530

    updated headline

--- src/App.js
+++ src/App.js
@@ -1,23 +1,28 @@
+import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import{Formik, Field, Form } from 'formik';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+       <h3> Signup form </h3>
+        <Formik 
+        initialValues={{ name: '', email:'',password: ''}}
+        onSubmit={(values) => 
+          {
+            console.log("values,", values)
+          }
+        }
         >
-          Learn React
-        </a>
-      </header>
+         <Form>
+           <Field name="name" placeholder="Enter name"/>
+           <Field name="email" type="email" placeholder="Enter email"/>
+           <Filed name="password" type="password" placeholder="Enter password"/>
+           <button type="submit"> Signup </button>
+           </Form> 
+        </Formik>
+
     </div>
   );
 }
